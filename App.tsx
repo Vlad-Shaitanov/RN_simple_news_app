fedaa57c1f806c7a327cd4e747f5d516936e4bdb
Vladislav Shaitanov <symphonyofhope@yandex.ru>
2023-11-14 20:49:01 +0300

    feat: Загрузка и отображение списка продуктов

--- App.tsx
+++ App.tsx
@@ -5,31 +5,85 @@
  * @format
  */
 
-import React from 'react';
-import {StatusBar, StyleSheet, Text, View} from 'react-native';
+import React, {useState, useEffect} from 'react';
+import {
+	StatusBar,
+	StyleSheet,
+	View,
+	Alert,
+	FlatList,
+	ActivityIndicator,
+	Text,
+	RefreshControl,
+} from 'react-native';
+import {Post} from './components/Post';
+import axios from 'axios';
 
 export default function App() {
+	const [products, setProducts] = useState<any>([]);
+	const [isLoading, setIsLoading] = useState<any>(true);
+
+	const getProducts = async () => {
+		setIsLoading(true);
+		axios
+			.get('https://fakestoreapi.com/products?limit=10')
+			.then(({data}) => {
+				setProducts(data);
+				console.log('data', data);
+			})
+			.catch(error => {
+				console.log('error', error);
+				Alert.alert('Ошибка', 'Не удалось получить список продуктов');
+			})
+			.finally(() => {
+				setIsLoading(false);
+			});
+	};
+
+	useEffect(() => {
+		getProducts();
+	}, []);
+
+	if (isLoading) {
+		return (
+			<View style={styles.productsDownload}>
+				<ActivityIndicator size="large" />
+				<Text style={styles.productsDownloadText}>Загрузка данных...</Text>
+			</View>
+		);
+	}
+
 	return (
-		<View style={styles.container}>
-			<Text style={styles.testText}>React Native app was started</Text>
+		<View>
+			{/* FlatList Нужен для рендера скроллируемых элементов */}
+			<FlatList
+				/* Пропс refreshControl нужен для обновления инфы по свайпу.
+				Когда свайпаем вниз и отпускаем экран, срабатывает событие onRefresh*/
+				refreshControl={
+					<RefreshControl refreshing={isLoading} onRefresh={getProducts} /> //refreshing это флаг, идет ли загрузка
+				}
+				data={products} //Пропс data принимает массив данных
+				renderItem={({item}: any) => (
+					//Пропс renderItem нужен для рендера элементов(Нужно обязательно деструктуризировать item, чтобы работало)
+					<Post
+						title={item.title}
+						imageUrl={item.image}
+						price={item.price + '$'}
+					/>
+				)}
+			/>
 			<StatusBar />
 		</View>
 	);
 }
 
 const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		backgroundColor: '#fff',
-		alignItems: 'center',
+	productsDownload: {
+		flex: 1, // чтобы было на всю ширину экрана
 		justifyContent: 'center',
+		alignItems: 'center',
 	},
-	testText: {
-		color: 'green',
-		fontWeight: 'bold',
-		fontSize: 24,
-	},
-	backgroundColor: {
-		color: 'green',
+	productsDownloadText: {
+		marginTop: 15,
 	},
 });
---
+++ components/Post.tsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import {StatusBar, StyleSheet, Text, View, Image} from 'react-native';
+
+export const Post = ({title, imageUrl, price}: any) => {
+	return (
+		<View>
+			<View style={styles.post}>
+				<Image
+					style={styles.postImage}
+					source={{
+						uri: imageUrl,
+					}}
+				/>
+				<View style={styles.postDetails}>
+					<Text style={styles.postTitle}>{title}</Text>
+					<Text style={styles.postDate}>{price}</Text>
+				</View>
+			</View>
+			<StatusBar />
+		</View>
+	);
+};
+
+const styles = StyleSheet.create({
+	post: {
+		flexDirection: 'row',
+		padding: 15,
+		borderBottomWidth: 1,
+		borderBottomColor: 'rgba(0, 0, 0, 0.1)',
+		borderStyle: 'solid',
+	},
+	postImage: {
+		height: 70,
+		width: 70,
+		borderRadius: 12,
+		marginRight: 12,
+	},
+	postTitle: {
+		fontSize: 16,
+		fontWeight: '700',
+	},
+	postDate: {
+		fontSize: 12,
+		color: 'rgba(0, 0, 0, 0.4)',
+		marginTop: 2,
+	},
+	postDetails: {
+		flex: 1,
+		justifyContent: 'center',
+	},
+});
